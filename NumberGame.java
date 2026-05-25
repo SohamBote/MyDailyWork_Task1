bf28bd401c2214f864444355188d89d492729170
Soham Bote <80t3soham@gmail.com>
2026-05-26 00:54:17 +0530

    Add NumberGame class for number guessing game

---
+++ NumberGame.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int number = rand.nextInt(100) + 1;
+        int guess = 0;
+
+        System.out.println("Guess number between 1 and 100");
+
+        while (guess != number) {
+            System.out.print("Enter guess: ");
+            guess = sc.nextInt();
+
+            if (guess > number) {
+                System.out.println("Too high!");
+            } else if (guess < number) {
+                System.out.println("Too low!");
+            } else {
+                System.out.println("Correct!");
+            }
+        }
+
+        sc.close();
+    }
+}
